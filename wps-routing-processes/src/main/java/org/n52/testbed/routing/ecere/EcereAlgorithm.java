ab866d892bab92029ff6912ae61aac757037c7e9
Christian Autermann <c.autermann@52north.org>
2019-08-26 11:38:04 +0200

    support maxHeight, maxWeight and intermediates for ecere

--- wps-routing-processes/src/main/java/org/n52/testbed/routing/ecere/EcereAlgorithm.java
+++ wps-routing-processes/src/main/java/org/n52/testbed/routing/ecere/EcereAlgorithm.java
@@ -16,14 +16,27 @@
  */
 package org.n52.testbed.routing.ecere;
 
-import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.MultiPoint;
+import org.locationtech.jts.geom.Point;
 import org.n52.javaps.algorithm.annotation.Algorithm;
 import org.n52.testbed.routing.DelegatingRoutingAlgorithm;
+import org.n52.testbed.routing.SupportsIntermediates;
+import org.n52.testbed.routing.SupportsMaxHeight;
+import org.n52.testbed.routing.SupportsMaxWeight;
 import org.n52.testbed.routing.model.routing.RouteDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 @Algorithm(identifier = "org.n52.routing.ecere", version = "1.0.0")
-public class EcereAlgorithm extends DelegatingRoutingAlgorithm {
+public class EcereAlgorithm extends DelegatingRoutingAlgorithm
+        implements SupportsIntermediates, SupportsMaxWeight, SupportsMaxHeight {
+    private MultiPoint intermediates;
+    private BigDecimal maxHeight;
+    private BigDecimal maxWeight;
 
     @Override
     protected RouteDefinition createRouteDefinition() {
@@ -31,7 +44,24 @@ public class EcereAlgorithm extends DelegatingRoutingAlgorithm {
         routeDefinition.setName(getName());
         routeDefinition.setPreference(getPreference());
         routeDefinition.setWaypoints(getWayPoints());
+        routeDefinition.setMaxHeight(getMaxHeight());
+        routeDefinition.setMaxWeight(getMaxWeight());
         return routeDefinition;
+    }
+
+    @Override
+    protected MultiPoint getWayPoints() {
+        Point[] points = Stream.of(Stream.of(getStartPoint()),
+                                   Optional.ofNullable(intermediates)
+                                           .map(mp -> IntStream.range(0, mp.getNumGeometries())
+                                                               .mapToObj(mp::getGeometryN)
+                                                               .map(Point.class::cast))
+                                           .orElseGet(Stream::empty),
+                                   Stream.of(getEndPoint()))
+                               .flatMap(Function.identity())
+                               .toArray(Point[]::new);
+
+        return getGeometryFactory().createMultiPoint(points);
     }
 
     @Override
@@ -39,4 +69,30 @@ public class EcereAlgorithm extends DelegatingRoutingAlgorithm {
         return "http://maps.ecere.com/route";
     }
 
+    @Override
+    public void setIntermediates(MultiPoint intermediates) {
+        this.intermediates = intermediates;
+    }
+
+    public MultiPoint getIntermediates() {
+        return intermediates;
+    }
+
+    @Override
+    public void setMaxHeight(BigDecimal maxHeight) {
+        this.maxHeight = maxHeight;
+    }
+
+    public BigDecimal getMaxHeight() {
+        return maxHeight;
+    }
+
+    @Override
+    public void setMaxWeight(BigDecimal maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public BigDecimal getMaxWeight() {
+        return maxWeight;
+    }
 }
